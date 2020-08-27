f5dcfc740d20f3f6ec577da28d703229b1a18524
Jacob Biggs <jbiggs@my.ancoraed.com>
2020-08-26 20:25:43 -0700

    Updated and working.

--- BlindDating/Controllers/DatingProfilesController.cs
+++ BlindDating/Controllers/DatingProfilesController.cs
@@ -6,147 +6,197 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BlindDating.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BlindDating.Controllers
 {
-    public class DatingProfilesController : Controller
-    {
-        private readonly BlindDatingContext _context;
+	public class DatingProfilesController : Controller
+	{
+		private readonly BlindDatingContext _context;
 
-        public DatingProfilesController(BlindDatingContext context)
-        {
-            _context = context;
-        }
+		private UserManager<IdentityUser> _userManager;
+		public DatingProfilesController(BlindDatingContext context, UserManager<IdentityUser> userManager)
+		{
+			_context = context;
+			_userManager = userManager;
+		}
 
-        // GET: DatingProfiles
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.DatingProfile.ToListAsync());
-        }
+		// GET: DatingProfiles
+		[Authorize(Roles = "Administrator")]
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.DatingProfile.ToListAsync());
+		}
 
-        // GET: DatingProfiles/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		[Authorize]
+		public async Task<IActionResult> Browse()
+		{
+			return View(await _context.DatingProfile.ToListAsync());
+		}
 
-            var datingProfile = await _context.DatingProfile
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (datingProfile == null)
-            {
-                return NotFound();
-            }
 
-            return View(datingProfile);
-        }
+		// GET: DatingProfiles/Details/5
+		[Authorize(Roles = "Administrator")]
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-        // GET: DatingProfiles/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+			var datingProfile = await _context.DatingProfile
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (datingProfile == null)
+			{
+				return NotFound();
+			}
 
-        // POST: DatingProfiles/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Age,Gender,Bio,UserAccountId")] DatingProfile datingProfile)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(datingProfile);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(datingProfile);
-        }
+			return View(datingProfile);
+		}
 
-        // GET: DatingProfiles/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		[Authorize]
+		public async Task<IActionResult> Show(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var datingProfile = await _context.DatingProfile.FindAsync(id);
-            if (datingProfile == null)
-            {
-                return NotFound();
-            }
-            return View(datingProfile);
-        }
+			var datingProfile = await _context.DatingProfile
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (datingProfile == null)
+			{
+				return NotFound();
+			}
 
-        // POST: DatingProfiles/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Age,Gender,Bio,UserAccountId")] DatingProfile datingProfile)
-        {
-            if (id != datingProfile.Id)
-            {
-                return NotFound();
-            }
+			return View(datingProfile);
+		}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(datingProfile);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!DatingProfileExists(datingProfile.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(datingProfile);
-        }
+		[Authorize]
+		public IActionResult ProfileInfo()
+		{
+			string userID = _userManager.GetUserId(User);
+			DatingProfile profile = _context.DatingProfile.FirstOrDefault(p => p.UserAccountId == userID);
 
-        // GET: DatingProfiles/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+			if (profile == null)
+			{
+				return RedirectToAction("Create");
+			}
 
-            var datingProfile = await _context.DatingProfile
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (datingProfile == null)
-            {
-                return NotFound();
-            }
+			return View(profile);
+		}
 
-            return View(datingProfile);
-        }
+		// GET: DatingProfiles/Create
+		[Authorize]
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: DatingProfiles/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var datingProfile = await _context.DatingProfile.FindAsync(id);
-            _context.DatingProfile.Remove(datingProfile);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		// POST: DatingProfiles/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[Authorize]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Age,Gender,Bio,UserAccountId")] DatingProfile datingProfile)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(datingProfile);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(datingProfile);
+		}
 
-        private bool DatingProfileExists(int id)
-        {
-            return _context.DatingProfile.Any(e => e.Id == id);
-        }
-    }
+		// GET: DatingProfiles/Edit/5
+		[Authorize(Roles = "Administrator")]
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var datingProfile = await _context.DatingProfile.FindAsync(id);
+			if (datingProfile == null)
+			{
+				return NotFound();
+			}
+			return View(datingProfile);
+		}
+
+		// POST: DatingProfiles/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Age,Gender,Bio,UserAccountId")] DatingProfile datingProfile)
+		{
+			if (id != datingProfile.Id)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(datingProfile);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!DatingProfileExists(datingProfile.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(datingProfile);
+		}
+
+		// GET: DatingProfiles/Delete/5
+		[Authorize(Roles = "Administrator")]
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var datingProfile = await _context.DatingProfile
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (datingProfile == null)
+			{
+				return NotFound();
+			}
+
+			return View(datingProfile);
+		}
+
+		// POST: DatingProfiles/Delete/5
+		[Authorize(Roles = "Administrator")]
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var datingProfile = await _context.DatingProfile.FindAsync(id);
+			_context.DatingProfile.Remove(datingProfile);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool DatingProfileExists(int id)
+		{
+			return _context.DatingProfile.Any(e => e.Id == id);
+		}
+	}
 }
--- BlindDating/Startup.cs
+++ BlindDating/Startup.cs
@@ -17,71 +17,71 @@ using BlindDating.Models;
 
 namespace BlindDating
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
 
-            services.AddDbContext<BlindDatingContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("BlindDating")));
+			services.AddDbContext<BlindDatingContext>(options =>
+				options.UseSqlServer(
+					Configuration.GetConnectionString("BlindDating")));
 
-            services.AddDbContext<SecurityContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("BlindDating")));
+			services.AddDbContext<SecurityContext>(options =>
+				options.UseSqlServer(
+					Configuration.GetConnectionString("BlindDating")));
 
-            services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<SecurityContext>()
-                .AddDefaultUI()
-                .AddDefaultTokenProviders();
+			services.AddIdentity<IdentityUser, IdentityRole>()
+				.AddEntityFrameworkStores<SecurityContext>()
+				.AddDefaultUI()
+				.AddDefaultTokenProviders();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, RoleManager<IdentityRole> roleManager,
-            UserManager<IdentityUser> userManager)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, RoleManager<IdentityRole> roleManager,
+			UserManager<IdentityUser> userManager)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseCookiePolicy();
 
-            app.UseAuthentication();
+			app.UseAuthentication();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
 
-            SetupSecurity.SeedRoles(roleManager);
-            SetupSecurity.SeedUsers(userManager);
-        }
-    }
+			SetupSecurity.SeedRoles(roleManager);
+			SetupSecurity.SeedUsers(userManager);
+		}
+	}
 }
